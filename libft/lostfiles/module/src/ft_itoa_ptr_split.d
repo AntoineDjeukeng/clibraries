f612c6ab19d1b8495b5a8a786c030ce2fcb46a55
AntoineDjeukeng <antoinedjeukeng.com>
2025-06-26 16:59:12 +0200

    only the lib folter works

--- libft/lostfiles/module/src/ft_itoa_ptr_split.d
+++
@@ -1,9 +0,0 @@
-src/ft_itoa_ptr_split.o: src/ft_itoa_ptr_split.c src/data_type.h \
- src/../ft_prinf.h src/../libft/libft.h src/../tests/tests_utils.h \
- src/../tests/../libft/libft.h src/../libft/libft.h
-src/data_type.h:
-src/../ft_prinf.h:
-src/../libft/libft.h:
-src/../tests/tests_utils.h:
-src/../tests/../libft/libft.h:
-src/../libft/libft.h:
--- libft/lostfiles/module/src/ft_printf_uint.d
+++
@@ -1,9 +0,0 @@
-src/ft_printf_uint.o: src/ft_printf_uint.c src/data_type.h \
- src/../ft_prinf.h src/../libft/libft.h src/../tests/tests_utils.h \
- src/../tests/../libft/libft.h src/../libft/libft.h
-src/data_type.h:
-src/../ft_prinf.h:
-src/../libft/libft.h:
-src/../tests/tests_utils.h:
-src/../tests/../libft/libft.h:
-src/../libft/libft.h:
